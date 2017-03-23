c750778f2a31309a89ac5db7444e6d61c18e32bf
Vojtech Horky <vojtech.horky@gmail.com>
2017-03-23 09:52:51 +0100

    Add helper functions for adding/updating via SQL

--- lib/db.php
+++ lib/db.php
@@ -56,12 +56,9 @@ function db_find_objects($description, $sql, $params = array()) {
 /**
  * Retrieve single result from the database.
  *
- * @param $description Query
- *            description, currently not used.
- * @param $sql SQL
- *            query with placeholders.
- * @param $params Actual
- *            parameters for the SQL query.
+ * @param $description Query description, currently not used.
+ * @param $sql SQL query with placeholders.
+ * @param $params Atual parameters for the SQL query.
  */
 function db_find_object($description, $sql, $params = array()) {
     $conn = option('db');
@@ -74,4 +71,98 @@ function db_find_object($description, $sql, $params = array()) {
     }
     
     return $result;
+}
+
+/**
+ * Prefix values with colon to be used in SQL binding.
+ * 
+ * @param $x Column names.
+ */
+function db_prepare_bind_value_for_insert($x) {
+    return ":" . $x;
+}
+
+/**
+ * Prepare condition for the WHERE clause of UPDATE statement.
+ *
+ * @param $x Column names.
+ */
+function db_prepare_bind_value_for_update($x) {
+    return sprintf("%s = :%s", $x, $x);
+}
+
+/**
+ * Create new database entry from given data.
+ * 
+ * @param $description Query description, currently not used.
+ * @param $object_array Associative array describing the new entry.
+ * @param $table Table name.
+ */
+function db_create_object_from_array($description, $object_array, $table) {
+    $columns = array_keys($object_array);
+
+    $columns_colons = array_map('db_prepare_bind_value_for_insert', $columns);
+
+    $sql = sprintf("INSERT INTO `%s` ( %s ) VALUES ( %s )",
+            $table, implode(', ', $columns), implode(', ', $columns_colons));
+
+    $conn = option('db');
+    $stmt = $conn->prepare($sql);
+    foreach ($columns as $c) {
+        $stmt->bindValue(':' . $c, $object_array[$c]);
+    }
+
+    $stmt->execute();
+}
+
+/**
+ * Update existing database entry with given data.
+ *
+ * @param $description Query description, currently not used.
+ * @param $object_array Associative array describing the new entry.
+ * @param $table Table name.
+ * @param $id_columns Column names identifying the object (primary keys).
+ */
+function db_update_object_from_array($description, $object_array, $table, $id_columns) {
+    $columns = array_keys($object_array);
+
+    if (!is_array($id_columns)) {
+        $id_columns = array($id_columns);
+    }
+
+    $columns_assignments = array_map('db_prepare_bind_value_for_update', $columns);
+    $id_columns_comparison = array_map('db_prepare_bind_value_for_update', $id_columns);
+
+    $sql = sprintf("UPDATE `%s` SET %s WHERE %s",
+            $table, implode(', ', $columns_assignments),
+            implode(' AND ', $id_columns_comparison));
+
+    $conn = option('db_conn');
+    $stmt = $conn->prepare($sql);
+    foreach ($columns as $c) {
+        $stmt->bindValue(':' . $c, $object_array[$c]);
+    }
+
+    $stmt->execute();
+}
+
+/**
+ * Delete a database entry.
+ *
+ * @param $description Query description, currently not used.
+ * @param $table Table name.
+ * @param $conditions Associative array naming removal conditions.
+ */
+function db_delete_objects($description, $table, $conditions) {
+    $comparisons = array_map('db_prepare_bind_value_for_update', array_keys($conditions));
+    $sql = sprintf("DELETE FROM `%s` WHERE %s",
+            $table, implode(' AND ', $comparisons));
+
+    $conn = option('db_conn');
+    $stmt = $conn->prepare($sql);
+    foreach ($conditions as $key => $value) {
+        $stmt->bindValue(':' . $key, $value);
+    }
+
+    $stmt->execute();
 }
